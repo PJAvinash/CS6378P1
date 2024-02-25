5e0167c1a7bad97ab1e95926ca1ed1322983f679
PJ Avinash <avinashindian2.0@gmail.com>
2024-02-25 12:43:48 -0600

    commit-0

--- Node.java
+++ Node.java
@@ -112,9 +112,9 @@ public class Node {
         this.bufferedMessages.sort(null);
         if(this.uid == 0){    
             if(bufferedMessages.size() > 0){
-                System.out.println(inputMessage.toString()+ " vc: "+ Arrays.toString(this.getVectorClock())+ " " + Arrays.toString(bufferedMessages.get(0).vectortimestamp)+" tid :"+ tid);
+                System.out.println(inputMessage.toString()+ " vc: "+ Arrays.toString(this.getVectorClock())+ " " + Arrays.toString(bufferedMessages.get(0).vectortimestamp))
             }else{
-                System.out.println(inputMessage.toString()+ " vc: "+ Arrays.toString(this.getVectorClock())+ " bufferedMessages: 0" + " tid :"+ tid);
+                System.out.println(inputMessage.toString()+ " vc: "+ Arrays.toString(this.getVectorClock())+ " bufferedMessages: 0");
             }
             
         }
